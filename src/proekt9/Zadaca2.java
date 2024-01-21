7618cec50956f708562f42244b7bf608ebab4e15
Jasin Ibishi <jasinibishi@gmail.com>
2024-01-21 14:56:54 +0100

    first commit

---
+++ src/proekt9/Zadaca2.java
@@ -0,0 +1,28 @@
+package proekt9;
+
+import java.util.Scanner;
+
+public class Zadaca2 {
+
+	public static void main(String[] args) {
+		Scanner tastatura = new Scanner(System.in);
+		int a,b,c,d;
+		int k=1;
+		System.out.println("Vnesi vrednost za A: ");
+		a=tastatura.nextInt();
+		System.out.println("Vnesi vrednost za B: ");
+		b=tastatura.nextInt();
+		System.out.println("Vnesi vrednost za C: ");
+		c=tastatura.nextInt();
+		if(a<=5 && b<(a-c))
+		{
+			System.out.println(k);
+		}
+		else if(c == 0)
+		{
+			d=0;
+			System.out.println(d);
+		}
+	}
+
+}
